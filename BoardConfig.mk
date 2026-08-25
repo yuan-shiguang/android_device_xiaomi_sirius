da359bfb45081e3ed809ac435529cf731cc781c1
yuan-shiguang <385325061@qq.com>
2026-08-25 21:50:04 +0800

    fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,6 +83,10 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 # Workaround for error copying vendor files to recovery ramdisk
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Broken Rules
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Recovery
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
